6a1b7fecdd8eb7d2378d19f7818c7a9c3e306696
Josh Pospisil <josh.pospisil@gmail.com>
2022-10-28 16:50:53 -0500

    Updated to make API request

--- demo-spring-boot-api/src/main/java/com/example/demoapi/security/AllowCrossOrigin.java
+++ demo-spring-boot-api/src/main/java/com/example/demoapi/security/AllowCrossOrigin.java
@@ -1,6 +1,5 @@
 package com.example.demoapi.security;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.lang.annotation.Documented;
