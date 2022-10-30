84a07c9e98681b5a56a309c7ed16fc0cb524d3cc
Josh Pospisil <josh.pospisil@gmail.com>
2022-10-30 13:55:29 -0500

    Added second realm and added multi-tenancy to api

--- demo-spring-boot-api/src/main/java/com/example/demoapi/DemoController.java
+++ demo-spring-boot-api/src/main/java/com/example/demoapi/DemoController.java
@@ -5,6 +5,7 @@ import com.example.demoapi.security.authorities.HasAuthorityDemoRead;
 import com.example.demoapi.security.authorities.HasAuthorityDemoWrite;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,17 +18,31 @@ public class DemoController {
     @GetMapping("/demo-read")
     @HasAuthorityDemoRead
     public Map<String, Object> demoRead() {
-        return Map.of("read-access", true);
+        var jwt = getJwt();
+        return Map.of(
+                "demo-endpoint", "read access demo endpoint",
+                "jwt issuer", jwt.getIssuer(),
+                "username", jwt.getClaims().get("preferred_username")
+        );
     }
 
     @GetMapping("/demo-write")
     @HasAuthorityDemoWrite
     public Map<String, Object> demoWrite() {
-        return Map.of("write-access", true);
+        var jwt = getJwt();
+        return Map.of(
+                "demo-endpoint", "write access demo endpoint",
+                "jwt issuer", jwt.getIssuer(),
+                "username", jwt.getClaims().get("preferred_username")
+        );
     }
 
     @GetMapping("/user-info")
     public Authentication userIngo() {
         return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    private Jwt getJwt() {
+        return (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
